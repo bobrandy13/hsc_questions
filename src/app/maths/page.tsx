64f2b822e09ccfdafc6051a88a45a230e71dea6c
Kevin Huang <khuang891@gmail.com>
2024-02-03 18:16:43 +1100

    created navbar
    with shadcn/ui

---
+++ src/app/maths/page.tsx
@@ -0,0 +1,9 @@
+export default function Maths() {
+  return (
+    <div className="h-screen w-screen">
+      <div className="flex h-3/4 w-screen items-center justify-center">
+        Maths
+      </div>
+    </div>
+  );
+}
